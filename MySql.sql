eab11b6e6995485c538b0bf4a43d2f3d55f9600a
Adriel Modollo <adrielmodollo@outlook.com>
2020-09-11 00:05:39 -0300

    Tabela verdade

--- MySql.sql
+++ MySql.sql
@@ -1,3 +1,12 @@
 SELECT * FROM itens_notas_fiscais WHERE QUANTIDADE > 60 AND PRECO <= 3
 
 SELECT * FROM tabela_de_clientes WHERE NOME LIKE '%Mattos'
+
+/*
+(NOT ((3 > 2) OR (4 >= 5)) AND (5 > 4) ) OR (9 > 0)
+(NOT ((Verdadeiro) OR (Falso)) AND (Verdadeiro) ) OR (Verdadeiro)
+(NOT (Verdadeiro) AND (Verdadeiro) ) OR (Verdadeiro)
+(Falso AND Verdadeiro) OR (Verdadeiro)
+(Falso) OR (Verdadeiro)
+Verdadeiro
+*/
