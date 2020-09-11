ccfa0be1ecd0da227fc485d83015d7170fbf2e26
Adriel Modollo <adrielmodollo@outlook.com>
2020-09-11 00:20:14 -0300

    Agrupamentos

--- MySql.sql
+++ MySql.sql
@@ -17,3 +17,14 @@ SELECT DISTINCT BAIRRO FROM tabela_de_clientes WHERE CIDADE = 'Rio de Janeiro'
 SELECT * FROM notas_fiscais  WHERE DATA_VENDA = '2017-01-01' limit 10
 --Ordena de forma decrescente ou ascendente
 SELECT * FROM itens_notas_fiscais WHERE codigo_do_produto = '1101035' ORDER BY QUANTIDADE DESC
+
+/*
+SUM: SOMA
+MAX: MÁXIMO
+MIN: MÍNIMO
+AVG: MÉDIA
+COUNT: CONTA OCORRÊNCIAS
+*/
+SELECT MAX(`QUANTIDADE`) as 'MAIOR QUANTIDADE' FROM itens_notas_fiscais WHERE `CODIGO_DO_PRODUTO` = '1101035' ;
+
+SELECT COUNT(*) FROM itens_notas_fiscais WHERE codigo_do_produto = '1101035' AND QUANTIDADE = 99;
